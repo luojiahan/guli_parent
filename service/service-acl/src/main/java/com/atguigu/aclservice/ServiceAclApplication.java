e0ddf132536cc5bf92606ad0c9a75a34cf893168
yiluohan1234 <1097189275@qq.com>
2022-05-19 13:36:48 +0800

    权限管理功能

---
+++ service/service-acl/src/main/java/com/atguigu/aclservice/ServiceAclApplication.java
@@ -0,0 +1,37 @@
+package com.atguigu.aclservice;
+
+import cn.hutool.core.util.StrUtil;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan("com.atguigu")
+@MapperScan("com.atguigu.aclservice.mapper")
+public class ServiceAclApplication {
+
+    public static void main(String[] args) throws UnknownHostException {
+
+        ConfigurableApplicationContext run= SpringApplication.run(ServiceAclApplication.class, args);
+        Environment env = run.getEnvironment();
+        String host = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        System.out.println(
+                StrUtil.format("\n----------------------------------------------------------\n\t" +
+                                "Application 'service-acl' is running! Access URLs:\n\t" +
+                                "Local: \t\thttp://localhost:{}\n\t" +
+                                "External: \thttp://{}:{}\n\t" +
+                                "Doc: \thttp://{}:{}/swagger-ui.html\n" +
+                                "----------------------------------------------------------",
+                        port, host, port, host, port));
+    }
+
+}
